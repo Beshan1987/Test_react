9ac272c3c2af759a7714dee89ffa1b858d01e8b9
User <user@MacBook-Pro.local>
2024-06-03 23:59:30 +0300

    feat: add sockets on the NewsListPage component

--- src/pages/NewsListPage/NewsListPage.tsx
+++ src/pages/NewsListPage/NewsListPage.tsx
@@ -4,22 +4,46 @@ import { Loader } from "~/shared/ui/Loader/Loader";
 import { ChangeEvent, useEffect, useState } from "react";
 import { NewsList } from "~/features/NewsList/NewsList";
 import debounce from "lodash/debounce";
+import { Socket, io } from "socket.io-client";
+import { News } from "~/entities/apiTypes";
+import { API_URL } from "~/store/api/constants";
 
 export const NewsListPage = () => {
   const { data: newsList, isLoading, error } = useGetNewsListQuery();
 
   const [filteredData, setFilteredData] = useState(newsList);
   const [filterDate, setFilterDate] = useState("");
+  const [socketNews, setSocketNews] = useState<News[] | null>(null); // tested version
+  const [socket, setSocket] = useState<Socket | null>(null); // tested version
+
+  useEffect(() => {
+    if (!socket) {
+      const newSocket = io(API_URL);
+      setSocket(newSocket);
+
+      newSocket.on("news", (data: News[]) => {
+        setSocketNews(data);
+      });
+
+      return () => {
+        newSocket.disconnect();
+      };
+    }
+  }, [socket]);
 
   useEffect(() => {
     const debouncedFilterData = debounce(() => {
-      filterDate && newsList
+      filterDate && socketNews
+        ? setFilteredData(
+            socketNews.filter((item) => item.title === filterDate) // need to change to date
+          )
+        : filterDate && newsList
         ? setFilteredData(newsList.filter((item) => item.title === filterDate))
         : setFilteredData(newsList);
     }, 500);
 
     debouncedFilterData();
-  }, [newsList, filterDate]);
+  }, [newsList, socketNews, filterDate]);
 
   const handleFilterDateChange = (event: ChangeEvent<HTMLInputElement>) => {
     setFilterDate(event.target.value);
@@ -31,6 +55,7 @@ export const NewsListPage = () => {
         <Loader />
       ) : (
         <div className={styles.wrapper}>
+          <div>{socketNews && socketNews[0].id}</div>
           <div className={styles.search}>
             <label>
               Filter by date:
@@ -43,8 +68,10 @@ export const NewsListPage = () => {
           </div>
           {filterDate ? (
             <NewsList newsList={filteredData} sliceAmount={20} /> // sliceAmount is an option, temporary
+          ) : socketNews ? (
+            <NewsList newsList={socketNews} sliceAmount={20} /> // // tested version of sockets, need to diving deeper
           ) : (
-            <NewsList newsList={newsList} sliceAmount={20} /> // sliceAmount is an option, temporary
+            <NewsList newsList={newsList} sliceAmount={20} />
           )}
           {error && "status" in error && (
             <div className={styles.error}>{error.status}</div>
